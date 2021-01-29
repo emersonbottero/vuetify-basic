e65167fce50ae998fef7d48e6f53c0254c038ba4
Emerson Bottero <emersonbottero@gmail.com>
2021-01-29 19:53:22 -0300

    added vuetify class components

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <p @click="sayHello">hello times: {{ helloTimes }}</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component({})
+export default class Hello extends Vue {
+  helloTimes = 0
+  sayHello() {
+      this.helloTimes++  
+  }
+
+  mounted() {
+    console.log('hello from hello');
+  }
+}
+</script>
